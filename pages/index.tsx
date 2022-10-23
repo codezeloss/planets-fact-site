bd0d7eb6f861dabc59861cc3b02ae5b7c2b5b352
Elhoussaine AIT AICHTE <elhoussaineaitX@gmail.com>
2022-10-23 14:00:33 +0100

    Project done 100%

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ const Home = () => {
   return (
     <div>
       <Head>
-        <html lang="en" />
         <title>The planets | Earth</title>
         <meta name="description" content="Home page" />
         <link rel="icon" href="/favicon.ico" />
